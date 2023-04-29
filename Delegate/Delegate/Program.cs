de3ca8fb2e9fa1785223456b5efb3b390782819d
usern23 <126382002+usern23@users.noreply.github.com>
2023-04-29 19:20:16 +0600

    Add files via upload

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿
+delegate double Maths(double x, double y, double z);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Maths summ = (x,y,z) => x + y + z;
+        Maths umnog = (x, y, z) => x * y * z;
+        Maths sred = (x,y,z) => (x + y + z)/3;
+        Maths maxvalue = (x, y, z) => Math.Max(Math.Max(x, y), z);
+        Maths minvalue = (x,y,z) => Math.Min(Math.Min(x, y), z);
+        Console.WriteLine("Ввелите элементы x | y | z");
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        Console.WriteLine("Сумма {0}", summ(a,b,c));
+        Console.WriteLine("Произведение {0}", umnog(a,b,c));
+        Console.WriteLine("Среднее арифметическое {0}", sred(a,b,c));
+        Console.WriteLine("Максимальное {0}", maxvalue(a,b,c));
+        Console.WriteLine("Минимальное {0}", minvalue(a,b,c));
+    }
+}
